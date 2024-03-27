d584eae1a4bbb47b587905611e5d0151a834de87
BugRuster <bugruster@gmail.com>
2024-03-27 23:38:58 +0530

    New Features added

--- lib/AppScreensMain/HomeScreen.dart
+++ lib/AppScreensMain/HomeScreen.dart
@@ -1,19 +1,95 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+void main() {
+  runApp(MSG_What());
+}
 
+class MSG_What extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            "Anurag",
-            style: TextStyle(fontSize: 36),
-          ),
-        ),
+      title: 'Flutter Bottom Navigation Bar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MSG_WhatHomePage(),
+    );
+  }
+}
+
+class MSG_WhatHomePage extends StatefulWidget {
+  @override
+  _MSG_WhatHomePageState createState() => _MSG_WhatHomePageState();
+}
+
+class _MSG_WhatHomePageState extends State<MSG_WhatHomePage> {
+  int _selectedIndex = 1; // Initially selected index for home button
+
+  final List<Widget> _screens = [
+    AddScreen(),
+    HomeScreen(),
+    SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bottom Navigation Bar Example'),
+      ),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class AddScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Add Screen'),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Home Screen'),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Settings Screen'),
     );
   }
 }
