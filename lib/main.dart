131c29c85159541ff58b9115d9977cc2813d7d1a
BugRuster <bugruster@gmail.com>
2024-03-31 22:57:25 +0530

    Firebase Added as Database

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'Login/LoginPage.dart';
 import 'AppScreensMain/TODO_1.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
